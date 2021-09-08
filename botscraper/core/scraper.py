28461bc42711e16852e2ce1d1a1c9e51d64eed0c
Jeferson Lima <jefersonjl82@gmail.com>
2021-09-08 13:47:40 -0300

    include filters and improve create_df method

--- botscraper/core/scraper.py
+++ botscraper/core/scraper.py
@@ -1,5 +1,7 @@
-# BotScraper Object
+''' BotScraper Object '''
 
+import ast
+import logging
 import configparser
 from dataclasses import dataclass, field
 from datetime import datetime
@@ -8,67 +10,97 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 @dataclass
 class Config:
     url: str = ''
     headers: str = ''
-    keywords: tuple = ()
+    output: str = ''
+    drops: dict = field(default_factory=dict)
+    keywords: list = field(default_factory=list)
     fields: list = field(default_factory=list)
+    alarm: list = field(default_factory=list)
 
 
 class ETL:
 
-    def __init__(self):
+    def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
         self.soup = None
         self.config = Config()
         self.load_cfg_file('config/configs.cfg')
-        self.config.fields = self.cfg['DATABASE']['FIELDS'].split(',')
+        self.config.fields = self.cfg['DATABASE']['fields'].split(',')
+        self.config.drops = ast.literal_eval(self.cfg['DATABASE']['drops'])
         self.df = pd.DataFrame()
+        self.last_df = None
+        self.c_news = []
 
-    def load_cfg_file(self, cfg_path):
+    def load_cfg_file(self, cfg_path) -> None:
         self.cfg = configparser.ConfigParser()
         self.cfg.read(cfg_path)
 
-    def extract_page_html(self, url):
+    def load_df(self) -> None:
+        try:
+            self.last_df = pd.read_csv(self.config.output)
+            self.last_df.fillna('', inplace=True)
+        except IOError:
+            self.logger.error("Error to load last_df")
+
+    def news_df(self):
+        try:
+            df_tmp = self.df[self.df['Keys'] != '']
+            df_url = df_tmp['URL'].drop_duplicates()
+            last_df_tmp = self.last_df[self.last_df['Keys'] != '']
+            last_df_url = last_df_tmp['URL'].drop_duplicates()
+            self.c_news = list(set(df_url) - set(last_df_url))
+        except KeyError:
+            self.logger.error("Please, make sure if ETL.run was called")
+
+    def drops_df(self):
+        for key in self.config.drops:
+            self.df = self.df[~self.df[key].str.contains(
+                self.config.drops[key])]
+
+    def create_df(self):
+        pass
+
+    def extract_page_html(self, url) -> (bool, BeautifulSoup):
         response = requests.get(url, headers={'User-Agent': self.config.headers})
         soup = BeautifulSoup(response.content, 'html.parser')
-        return soup
+        return response.ok, soup
 
     @staticmethod
-    def r_tags(element):
+    def r_tags(element) -> str:
         remove_tags = re.compile(r'<[^>]+>')
         out = remove_tags.sub('', str(element))
         out = out.replace('\n', '')
         out = out.replace(';', '')
         return out
 
-    @staticmethod
-    def try_date(date_str):
+    def try_date(self, date_str) -> str:
         format_str = '%d/%m/%Y'
         try:
             out = datetime.strptime(date_str, format_str)
-        except ValueError as e:
-            print(f"Datetime error: {e}")
+        except ValueError:
+            self.logger.error("Datetime error!")
             out = datetime.fromtimestamp(0).strftime(format_str)
         return out
 
-    def create_df(self) -> None:
-        pass
-
-    def load_page(self) -> None:
-        pass
-
-    def storage(self):
+    def storage(self) -> None:
         try:
-            self.df.to_csv('output/contest_out.csv', index=False)
-        except  IOError as e:
-            print(f"Error to Storage: {e}")
+            self.df.to_csv(self.config.output, index=False)
+        except  IOError:
+            self.logger.error("Error to Storage!")
 
-    def run(self):
+    def run(self) -> None:
         # Extract
-        self.soup = self.extract_page_html(self.config.url)
+        _, self.soup = self.extract_page_html(self.config.url)
         # Transform
-        self.create_df()
+        self.create_df()    # create database
+        self.drops_df()     # drop values
+        self.news_df()      # get news
         # Load
-        self.storage()
+        self.storage()      # storage database
+        self.load_df()      # update values
