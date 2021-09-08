65b98335d8f49ba700c8577d3caacdb65deddfda
Jeferson Lima <jefersonjl82@gmail.com>
2021-09-08 13:48:14 -0300

    remove unless code

--- botscraper/scrapers/pci.py
+++ botscraper/scrapers/pci.py
@@ -1,18 +1,19 @@
 ''' ETL Objects '''
 
-from ..core import scraper
+from ..bot import telegram
 
-
-class PCIContest(scraper.ETL):
+class PCIContest(telegram.TBot):
 
     def __init__(self):
         super().__init__()
-        self.load_page()
+        self.load_cfg()
+        self.load_df()
 
-    def load_page(self):
-        self.config.url = str(self.cfg['PCI']['URL_BASE'])
-        self.config.headers = str(self.cfg['PCI']['HEADERS'])
-        self.config.keywords = tuple(self.cfg['GERAL']['keywords'].split(','))
+    def load_cfg(self):
+        self.config.url = str(self.cfg['PCI']['url_base'])
+        self.config.headers = str(self.cfg['PCI']['headers'])
+        self.config.output = str(self.cfg['PCI']['output'])
+        self.config.keywords = self.cfg['GERAL']['keywords'].split(',')
 
     def create_df(self):
         contests = self.soup.find_all("div", attrs={"class": "ca"})
@@ -24,19 +25,21 @@ class PCIContest(scraper.ETL):
             c_until.append(date_str)
             c_title.append(cnt.select('a')[0].get('title'))
             c_url.append(cnt.select('a')[0].get('href'))
-        c_keys = self.check_keywords(c_url)
-        self.df[self.config.fields] = list(zip(c_role, c_state, c_until, c_title, c_url, c_keys))
+        c_keys, c_txt_body = self.check_keywords(c_url)
+        self.df = self.df.iloc[0:0]
+        self.df[self.config.fields] = list(zip(c_role, c_state, c_until,
+                                               c_title, c_url, c_keys, c_txt_body))
+        self.df.fillna('', inplace=True)
 
     def check_keywords(self, url_list):
-        c_keys = []
+        c_keys, c_txt_body = [], []
         for url in url_list:
-            print(f"Checking keywords in: {url}")
             body_soup = self.extract_page_html(url)
             txt_body = self.r_tags(body_soup.find("div", {"itemprop": "articleBody"})).lower()
+            c_txt_body.append(txt_body)
             keys = ''
             for key in self.config.keywords:
                 if key in txt_body:
-                    keys = keys + key + ' '
-            c_keys.append(keys)
-            print(keys)
-        return c_keys
+                    keys = keys + key + ', '
+            c_keys.append(keys[:-2])
+        return c_keys, c_txt_body
